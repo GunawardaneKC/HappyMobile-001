747d852c13eb7fcfbc87742b1f127029e12a9bf7
IT21274466IO <it21274466@my.sliit.lk>
2023-05-13 22:32:34 +0530

    5th Commit

--- client/src/components/mainpages/auth/Login.js
+++ client/src/components/mainpages/auth/Login.js
@@ -1,6 +1,10 @@
-import React, {useState} from 'react'
-import {Link} from 'react-router-dom'
-import axios from 'axios'
+import React, {useState} from 'react';
+import {Link} from 'react-router-dom';
+import axios from 'axios';
+import {FaUserShield} from 'react-icons/fa';
+import {BsFillShieldLockFill} from 'react-icons/bs';
+import video from '../../../images/polygon-145031.mp4';
+// import elaLogo from '../../../images/logo-hm.png';
 
 function Login() {
     const [user, setUser] = useState({
@@ -26,21 +30,64 @@ function Login() {
     }
 
     return (
-        <div className="login-page">
-            <form onSubmit={loginSubmit}>
-                <h2>Login</h2>
-                <input type="email" name="email" required
-                placeholder="Email" value={user.email} onChange={onChangeInput} />
+        <div className='loginPageLR flexLR'>
+        <div className="containerLR flexLR">
 
-                <input type="password" name="password" required autoComplete="on"
-                placeholder="Password" value={user.password} onChange={onChangeInput} />
+            <div className="videoDiv">
+                <video src={video} autoPlay muted loop></video>
 
-                <div className="row">
-                    <button type="submit">Login</button>
-                    <Link to="/register">Register</Link>
+                <div className='textDiv'>
+                    <h2 className='title'>Buy And Rent Mobile Phones</h2>
+                    <p>Adopt best quality Brand new Mobiles and Used ones.</p>
                 </div>
-            </form>
+
+                <div className="footerDiv flexLR">
+                    <span className='text'>Don't have an account?</span>
+                    <Link to={'/register'}>
+                        <button className='btn font-secondary'>Sign Up</button>
+                    </Link>
+                </div>
+            </div>    
+            
+            <div className="formDiv flexLR">
+                <div className="headerDiv">
+                    {/* <img src={elaLogo} alt="elaLogo" /> */}
+                    <h3>Welcome Back!</h3>
+                </div>
+
+                <form noValidate onSubmit={loginSubmit} className='form grid'>
+                    <span className='showMessage'>Login Status will go here</span>
+
+                    <div className="inputDiv">
+                        <label htmlFor="email">Email:</label>
+                        <div className="input flexLR">
+                            <FaUserShield className='icon' />
+                            <input type="text" id="email" name="email" placeholder="Enter Email" value={user.email} onChange={onChangeInput} />
+                        </div>
+                    </div>
+
+                    <div className="inputDiv">
+                        <label htmlFor="password">Password:</label>
+                        <div className="input flexLR">
+                            <BsFillShieldLockFill className='icon' />
+                            <input type="password" id="password" name="password" placeholder="Enter Password" value={user.password} onChange={onChangeInput} />
+                        </div>
+                    </div>
+
+                    <button type='submit' className='btn flexLR'>
+                        <span className='font-secondary'>Login</span>
+                        {/* <AiOutlineSwapRight className='icon' /> */}
+                    </button>
+
+                    <span className='forgotPassword'>
+                        Forgot your password? <a href="/">Click Here</a>
+                    </span>
+
+                </form>
+            </div>
+
         </div>
+      </div>
     )
 }
 
--- client/src/components/mainpages/auth/Register.js
+++ client/src/components/mainpages/auth/Register.js
@@ -1,6 +1,11 @@
-import React, {useState} from 'react'
-import {Link} from 'react-router-dom'
-import axios from 'axios'
+import React, {useState} from 'react';
+import {Link} from 'react-router-dom';
+import axios from 'axios';
+import {FaUserShield} from 'react-icons/fa';
+import {BsFillShieldLockFill} from 'react-icons/bs';
+import video from '../../../images/polygon-145031.mp4';
+// import elaLogo from '../../../images/logo-hm.png';
+import {MdMarkEmailRead} from 'react-icons/md';
 
 function Register() {
     const [user, setUser] = useState({
@@ -27,23 +32,85 @@ function Register() {
     }
 
     return (
-        <div className="login-page">
-            <form onSubmit={registerSubmit}>
-                <h2>Register</h2>
-                <input type="text" name="name" required
-                placeholder="User Name" value={user.name} onChange={onChangeInput} />
+        <div className='registerPageLR flexLR'>
+        <div className="containerLR flexLR">
 
-                <input type="email" name="email" required
-                placeholder="Email" value={user.email} onChange={onChangeInput} />
+            <div className="videoDiv">
+                <video src={video} autoPlay muted loop></video>
 
-                <input type="password" name="password" required autoComplete="on"
-                placeholder="Password" value={user.password} onChange={onChangeInput} />
-
-                <div className="row">
-                    <button type="submit">Register</button>
-                    <Link to="/login">Login</Link>
+                <div className='textDiv'>
+                    <h2 className='title'>Buy And Rent Mobile Phones</h2>
+                    <p>Adopt best quality Brand new Mobiles and Used ones.</p>
                 </div>
-            </form>
+
+                <div className="footerDiv flexLR">
+                    <span className='text'>Have an account?</span>
+                    <Link to={'/login'}>
+                        <button className='btn font-secondary'>Login</button>
+                    </Link>
+                </div>
+            </div>    
+            
+            <div className="formDiv flexLR">
+                <div className="headerDiv">
+                    {/* <img src={elaLogo} alt="elaLogo" /> */}
+                    <h3>Let us Know You!</h3>
+                </div>
+
+                <form noValidate onSubmit={registerSubmit} className='form grid'>
+
+                    {/* <div className="inputDiv">
+                            <label htmlFor="first_name">First Name</label>
+                            <div className="input flexLR">
+                                <FaUserShield className='icon' />
+                                <input type="text" name='first_name' placeholder="Enter First Name" value={this.state.first_name} onChange={this.onChange} />
+                            </div>
+                    </div>
+
+                    <div className="inputDiv">
+                            <label htmlFor="last_name">Last Name</label>
+                            <div className="input flexLR">
+                                <FaUserShield className='icon' />
+                                <input type="text" name="last_name" placeholder="Enter Last Name" value={this.state.last_name} onChange={this.onChange}/>
+                            </div>
+                    </div> */}
+                    <div className="inputDiv">
+                            <label htmlFor="last_name">Username</label>
+                            <div className="input flexLR">
+                                <FaUserShield className='icon' />
+                                <input type="text" name="name" placeholder="Enter Username" value={user.name} onChange={onChangeInput}/>
+                            </div>
+                    </div>
+
+                    <div className="inputDiv">
+                        <label htmlFor="email">Email</label>
+                        <div className="input flexLR">
+                            <MdMarkEmailRead className='icon' />
+                            <input type="email" id="email" name="email" placeholder="Enter Email" value={user.email} onChange={onChangeInput} />
+                        </div>
+                    </div>
+
+                    <div className="inputDiv">
+                        <label htmlFor="password">Password</label>
+                        <div className="input flexLR">
+                            <BsFillShieldLockFill className='icon' />
+                            <input type="password" id="password" name="password" placeholder="Enter Password" value={user.password} onChange={onChangeInput}/>
+                        </div>
+                    </div>
+
+                    <button type='submit' className='btn flexLR'>
+                        <span className='font-secondary'>Register</span>
+                        {/* <AiOutlineSwapRight className='icon' /> */}
+                    </button>
+
+                    {/* <span className='forgotPassword'>
+                        Forgot your password? <a href="/">Click Here</a>
+                    </span> */}
+
+                </form>
+            </div>
+
+        </div>
         </div>
     )
 }
