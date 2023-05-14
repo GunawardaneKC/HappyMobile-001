34fff3659695799abc212e7e5605d2e3daf08b27
IT21274466IO <it21274466@my.sliit.lk>
2023-05-14 22:09:08 +0530

    10th commit

--- client/src/components/mainpages/history/Allorders.js
+++ client/src/components/mainpages/history/Allorders.js
@@ -65,92 +65,112 @@ const Ord = () => {
   };
 
   return (
-    <div className='container'>
-      <div className="row">
-        <div className="col-lg-9 mt-2 mb-2">
-        </div>
-        <div className="col-lg-3 mt-2 mb-2">
-          <input className='form-control' type="search" placeholder='Search' name='searchQuery' onChange={handleSearch} />
-        </div>
+    <div className="mt-8 mx-auto max-w-7xl text-gray-950">
+  <div className="flex justify-between items-center">
+    <div className="flex-1">
+      <div className="relative text-gray-600 focus-within:text-gray-400">
+        <span className="absolute inset-y-0 text-center-0 flex items-center pl-2">
+          <i className="fas fa-search"></i>
+        </span>
+        <input
+          className="py-2 w-64 pl-10 placeholder-gray-500 rounded-md border border-gray-200 focus:outline-none focus:ring-2 focus:ring-blue-600 focus:border-transparent"
+          type="search"
+          placeholder="Search"
+          name="searchQuery"
+          onChange={handleSearch}
+        />
       </div>
-      <div>
-        <button className="btn btn-success"><Link to='/add/emp' style={{textDecoration: 'none', color:'black'}}>Add New Employee</Link></button>&nbsp;
-      </div>
-      <h3 style={{ marginTop: '40px', marginBottom: '-30px'}}>Employees</h3>
-      <table className='table table-hover' style={{ marginTop: '40px' }}>
-        <thead>
-          <tr>
-            <th scope='col'>No</th>
-            <th scope='col'>Order ID</th>
-            <th scope='col'>Name</th>
-            <th scope='col'>Email Address</th>
-            <th scope='col'>Payment</th>
-            <th scope='col'>Details</th>
-            <th scope='col'></th>
-          </tr>
-        </thead>
-        <tbody>
-          {posts.map((post, index) => (
-            <tr key={post._id}>
-              <th scope='row'>{index + 1}</th>
-              <td>{post._id}</td>
-              <td>{post.name}</td>
-              <td>{post.email}</td>
-              <td>
-                <select
-                  className="form-select"
-                  aria-label="Default select example"
-                  value={post.payment}
-                  onChange={(e) => {
-                    const value = e.target.value;
-                    const id = post._id;
-                    axios.put(`/payment/update/${id}`, { payment: value })
-                      .then((response) => {
-                        console.log(response.data);
-                        retrievePosts();
-                      })
-                      .catch((error) => {
-                        console.log(error);
-                      });
-                  }}
-                >
-                  <option value="Pending">Pending</option>
-                  <option value="Paid">Paid</option>
-                </select>
-              </td>
-              <td>
-                <table style={{ margin: "30px 0px" }}>
-                  <thead>
-                    <tr>
-                      <th>Product ID</th>
-                      <th>Products</th>
-                      <th>Quantity</th>
-                      <th>Price</th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    {post.cart.map((item) => (
-                      <tr key={item._id}>
-                        <td>{item.product_id}</td>
-                        <td>{item.title}</td>
-                        <td>{item.quantity}</td>
-                        <td>LKR {item.price * item.quantity}</td>
-                      </tr>
-                    ))}
-                  </tbody>
-                </table>
-              </td>
-              <td>
-                &nbsp;
-                <button className='btn btn-danger' onClick={() => deletePost(post._id)}>
-                  <i className='fas fa-trash-alt'></i>&nbsp;Delete
-                </button>
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
     </div>
+    <div>
+      <button className="ml-14 py-2 px-4 bg-blue-500 hover:bg-blue-600 text-white rounded-md">
+        <Link to="/add/emp" style={{ textDecoration: "none" }}>
+          Add New Order
+        </Link>
+      </button>
+    </div>
+  </div>
+  <h3 className="text-2xl mt-8 mb-4 font-bold text-zinc-50">Orders</h3>
+  <table className="min-w-full divide-y divide-gray-200">
+  <thead className='bg-blue-500'>
+    <tr>
+      <th className="px-6 py-3 text-center text-gray-900 uppercase tracking-wider">#
+      </th>
+      <th className="px-6 py-3 text-center text-gray-900 uppercase tracking-wider">Order ID
+      </th>
+      <th className="px-6 py-3 text-center text-gray-900 uppercase tracking-wider">Name
+      </th>
+      <th className="px-6 py-3 text-center text-gray-900 uppercase tracking-wider">Email Address
+      </th>
+      <th className="px-6 py-3 text-center text-gray-900 uppercase tracking-wider">Payment
+      </th>
+      <th className="px-6 py-3 text-center text-gray-900 uppercase tracking-wider">Details
+      </th>
+      <th className="px-6 py-3">Action</th>
+    </tr>
+  </thead>
+  <tbody className="bg-blue-200 divide-y divide-gray-200">
+    {posts.map((post, index) => (
+      <tr key={post._id}>
+        <td className="px-6 py-4 whitespace-nowrap">{index + 1}</td>
+        <td className="px-6 py-4 whitespace-nowrap">{post._id}</td>
+        <td className="px-6 py-4 whitespace-nowrap">{post.name}</td>
+        <td className="px-6 py-4 whitespace-nowrap">{post.email}</td>
+        <td className="px-6 py-4 whitespace-nowrap">
+          <select
+            className="border border-gray-300 rounded-md text-gray-500 py-1 px-3"
+            aria-label="Default select example"
+            value={post.payment}
+            onChange={(e) => {
+              const value = e.target.value;
+              const id = post._id;
+              axios
+                .put(`/payment/update/${id}`, { payment: value })
+                .then((response) => {
+                  console.log(response.data);
+                  retrievePosts();
+                })
+                .catch((error) => {
+                  console.log(error);
+                });
+            }}
+          >
+            <option value="Pending">Pending</option>
+            <option value="Paid">Paid</option>
+          </select>
+        </td>
+        <td className="px-6 py-4 whitespace-nowrap">
+          <table className="table-auto">
+            <thead>
+              <tr className="text-slate-500">
+                <th className="">Product ID</th>
+                <th className="">Products</th>
+                <th className="">Quantity</th>
+                <th className="">Price</th>
+              </tr>
+            </thead>
+            <tbody>
+              {post.cart.map((item) => (
+                <tr key={item._id} className="">
+                  <td className="">{item.product_id}</td>
+                  <td className="">{item.title}</td>
+                  <td className="">{item.quantity}</td>
+                  <td className="">LKR {item.price * item.quantity}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </td>
+        <td className="px-6 py-4 whitespace-nowrap">
+          &nbsp;
+          <button className="bg-red-500 hover:bg-red-700 text-white font-bold py-1 px-2 rounded focus:outline-none focus:shadow-outline" onClick={() => deletePost(post._id)}>
+                <i className='fas fa-trash-alt'></i>&nbsp;Delete
+              </button>
+            </td>
+          </tr>
+        ))}
+      </tbody>
+  </table>
+  </div>
   );
 }
   export default Ord;
